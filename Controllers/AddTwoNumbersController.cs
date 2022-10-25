8983b591ee26d7647147f53d0428b344fb3e55a8
ricRobo4 <artistr3@proton.me>
2022-10-25 13:20:24 -0700

    added comments

--- Controllers/AddTwoNumbersController.cs
+++ Controllers/AddTwoNumbersController.cs
@@ -1,3 +1,8 @@
+// Richard Johnson
+// 10-25-22
+// This API will take the numbers the user puts in the URL and add them together, before printing the sum to the player
+// Peer Review: 
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace JohnsonRAddTwoNumbers.Controllers;
@@ -11,16 +16,20 @@ public class AddTwoNumbersController : ControllerBase
 
     public int AddingNumbers(string input1, string input2)
     {
+        // This is where the variables are declared
         int num1 = 0;
         int num2 = 0;
         int sum = 0;
         bool isNumber;
+        // This will take the first number the user puts in the URL and save it as a string, which will be converted to an int
         isNumber = Int32.TryParse(input1, out num1);
             if(isNumber == true)
             {
+                // If the input was a valid number that could be converted to an int, the program will do the same with the second number
                 isNumber = Int32.TryParse(input2, out num2);
                     if(isNumber == true)
                     {
+                        // If both numbers are confirmed valid, the program will add them both together and save the sum as an int
                         sum = num1 + num2;
                     }
                     else
@@ -32,6 +41,7 @@ public class AddTwoNumbersController : ControllerBase
             {
                 sum = 000;
             }
+        // After the sum is saved as an int, the program will return the sum to the user
         return sum;
     }
 }
